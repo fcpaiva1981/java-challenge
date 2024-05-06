272cae6913e6790bf64962f3571aab3d158a402a
Fernando Campos de Paiva <fernandocampospaiva@hotmail.com>
2024-05-05 23:46:57 -0300

    Usuaários

--- src/main/java/br/com/fcpaiva/javachallenge/dto/UsuarioDto.java
+++ src/main/java/br/com/fcpaiva/javachallenge/dto/UsuarioDto.java
@@ -1,2 +1,33 @@
-package br.com.fcpaiva.javachallenge.dto;public class UsuarioDto {
+package br.com.fcpaiva.javachallenge.dto;
+
+import br.com.fcpaiva.javachallenge.convert.EntityOutputConvert;
+import br.com.fcpaiva.javachallenge.entity.UsuarioEntity;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+import java.util.List;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuarioDto {
+    private Long id;
+    private String nome;
+    private String email;
+
+    public static UsuarioEntity toEntity(UsuarioDto usuarioDto){
+        UsuarioEntity usuarioEntity = EntityOutputConvert.convert(usuarioDto, UsuarioEntity.class);
+        return usuarioEntity;
+    }
+
+    public static List<UsuarioDto> fromEntityList(List<UsuarioEntity> usuarioEntityList) {
+        return usuarioEntityList.stream().map(UsuarioDto::fromEntity).toList();
+    }
+
+    public static UsuarioDto fromEntity(UsuarioEntity usuarioEntity) {
+        var usuarioDto = EntityOutputConvert.convert(usuarioEntity, UsuarioDto.class);
+        return usuarioDto;
+    }
 }
