1353c830a824ff49e661d6a8747a8205725a098f
Fernando Campos de Paiva <fernandocampospaiva@hotmail.com>
2024-05-05 22:14:12 -0300

    Início do projeto

---
+++ src/test/java/br/com/fcpaiva/javachallenge/JavachallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fcpaiva.javachallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavachallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
