6440fe6267c80ae561d30635d881669f1169abe1
Fernando Campos de Paiva <fernandocampospaiva@hotmail.com>
2024-05-07 22:00:41 -0300

    Deixando somente banco para o Java

--- DockerCompose/data.sql
+++ DockerCompose/data.sql
@@ -1,10 +1,3 @@
-create database chsharpchallenge;
-use chsharpchallenge;
-create table if not exists Usuarios(Id    int auto_increment primary key,Nome  varchar(255) not null,Email varchar(255) not null);
-create table if not exists Permissaos(Id int auto_increment primary key,Descricao varchar(255) not null, UsuarioId int null, constraint FK_Permissaos_Usuarios_UsuarioId foreign key (UsuarioId) references Usuarios (Id));
-create index IX_Permissaos_UsuarioId on Permissaos (UsuarioId);
-
-
 create database javachallenge;
 use javachallenge;
 create table flyway_schema_history(installed_rank int  not null primary key,version  varchar(50)  null,description  varchar(200)  not null,type  varchar(20) not null,script  varchar(1000) not null,checksum int null,installed_by  varchar(100) not null,installed_on  timestamp default CURRENT_TIMESTAMP not null,execution_time int not null,success tinyint(1) not null);
